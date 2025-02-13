64b351f9b1b05f34097c5d8402d2fb94a749b098
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-02-13 16:08:27 +0600

    setup user model admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Use 'email' instead of 'username'
+    ordering = ['email']
+    list_display = ['email', 'first_name', 'last_name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')}
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
