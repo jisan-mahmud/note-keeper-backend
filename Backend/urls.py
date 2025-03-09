27fc7ff024f24d0f6f470069cf93c0e5510f3a8a
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-03-10 01:46:11 +0600

    configure static file

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +10,4 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls.jwt')),
     path('api/notes/', include('notes.urls')),
     path('api/tags/', include('tags.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
